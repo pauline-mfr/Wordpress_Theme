41ef05727fad8ac54cb0d19d93c593deccb86c29
pauline-mfr <p.monfrance@codeur.online>
2020-08-28 16:19:43 +0200

    Taxonomies

--- archive-portfolio.php
+++ archive-portfolio.php
@@ -1,8 +1,6 @@
 <?php get_header(); ?>
 <h1><?php post_type_archive_title(); ?></h1>
-<article class="container py-3">
-
-<?php the_terms( get_the_ID() , 'project-type' ); ?>
+<article class="container py-3" id="portfolio">
 
 <div class="row row-cols-1 row-cols-md-3 g-4 pt-5" data-masonry='{"percentPosition": true }'>
 <?php if (have_posts()) : ?>
@@ -12,7 +10,8 @@
 
 	<div class="card-img px-0" id="portfolio-img"><a href="<?php the_permalink() ?>"><?php the_post_thumbnail('medium', ['class' => 'card-img img-fluid', 'alt' => '', 'style' => 'height: auto;']) ?></a></div>
   <div class="card-img-overlay">
-    <h4 class="card-title pb-3  px-3" id="portfolio-title"><?php the_title() ?></h4>
+		<p class="taxonomy"><?php the_terms( get_the_ID() , 'project-type' ); ?></p>
+    <h4 class="card-title py-3 px-3 text-center" id="portfolio-title"><?php the_title() ?></h4>
   </div>
 
 </div>
@@ -24,13 +23,9 @@
 </div>
 </article>
 
-
-
-
 	<div class="pages-nav d-flex justify-content-center py-5">
 <?php the_posts_pagination(); ?>
 </div>
-
 
 <?php get_footer(); ?>
 
---
+++ portfolio.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: First plugin
+Plugin URI:
+Description: This is my first plugin
+Author: My name
+Version: 1.0
+Author URI: 
+*/
